f82dda872baaafc49877dd041018d462f8998d7a
JoaoNini75 <105883412+JoaoNini75@users.noreply.github.com>
2022-05-26 19:34:49 +0100

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -6,28 +6,60 @@ FROM userA inner join transactionA using (transactionId)
 WHERE softwareName = 'Grand Theft Auto V' and recipientId != userId;
 
 
--- Quais os nomes e classificações dos jogos da plataforma categorizados como compatíveis para Windows (tag), de simulação (tag) e com uma dada classificação etária? 
-select softwareName, classification
-from software inner join defined using(softwareId) inner join tag using(tagId)
+-- Quais os nomes e classificações dos jogos da plataforma categorizados como compatíveis para Windows 
+--(tag), de simulação (tag) e com uma dada classificação etária? (classificação etária = 12)
 
-
-where tagName = 'Compatible with Windows' and tagName = 'Simulation'
+(select softwareName, classification
+from software inner join defined using (softwareId)
+              inner join tag using (tagId)
+where tagName = 'Compatible with Windows' and ageRating <= 12)
+intersect
+(select softwareName, classification
+from software inner join defined using (softwareId)
+              inner join tag using (tagId)
+where tagName = 'Simulation' and ageRating <= 12);
+ 
 
 -- Qual o nome e e-mail do utilizador com mais transações feitas na plataforma?
-SELECT fullName, email
-FROM
 
--- Qual o nome do jogo associado à DLC com a melhor classificação na plataforma?
-SELECT softwareName, softwareName, MAX(classification)
-FROM software inner join expands using (softwareId)
 
---FETCH FIRST 1 ROWS ONLY;
+-- Qual o nome do jogo associado à DLC com a melhor classificação na plataforma?4
+with dlcs as (
+    select dlc
+    from expands
+)
+with bestDlc as (
+    select dlc, softwareName as dlcName, classification
+    from dlcs inner join software using (dlc)
+    where classification in (select max(classification) from software)
+)
+select sofwareName as basegameName, dlcName, classification 
+from bestDlc inner join expands using(dlc)
+             inner join software using(basegame);
+
+
+
+
 
 -- Quais os nomes, descrições, preços e datas de lançamento dos jogos lançados por editoras portuguesas que já foram comprados pelo menos uma vez? 
 
  
 
+-- 12.
+-- Qual o nome do candidato à FCT com maior média do secundário, e qual essa média? 
+select nome, mediaSec/10
+from candidatos, (select max(mediaSec) as mediaMax from candidatos) A
+where A.mediaMax = mediaSec;
 
+-- ou
+select nome, mediaSec/10
+from candidatos
+where mediaSec = all (select max(mediaSec) from candidatos);
+
+-- ou
+select nome, mediasec/10
+from candidatos 
+where mediaSec in (select max(mediaSec) from candidatos);
 
 
  
